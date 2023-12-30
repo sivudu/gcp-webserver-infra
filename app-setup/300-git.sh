b766ad6b0d7a73d3597ebb147874fd6bf88bf44a
sivudu <shivadev@outlook.com>
2023-12-29 22:02:01 -0800

    initial

---
+++ app-setup/300-git.sh
@@ -0,0 +1,3 @@
+cd ~
+git clone https://github.com/sivudu/gcp-webserver-infra.git gcp-webserver-infra
+
---
+++ infra/create-vm.cmd
@@ -0,0 +1,18 @@
+gcloud compute instances create sivashankar-dev-web-server \
+    --project=website-376021 \
+    --zone=us-west1-b \
+    --machine-type=e2-small \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --no-restart-on-failure \
+    --maintenance-policy=TERMINATE \
+    --provisioning-model=SPOT \
+    --instance-termination-action=STOP \
+    --service-account=316452042845-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+    --tags=http-server,https-server \
+    --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/debian-cloud/global/images/debian-11-bullseye-v20231212,mode=rw,size=40,type=projects/website-376021/zones/us-west1-b/diskTypes/pd-standard \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
